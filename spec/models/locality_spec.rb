93e7b7ea0e3778bf6b239bc0a627c90a4178220c
VinyPinheiro <viny-pinheiro@hotmail.com>
2019-07-15 20:06:19 -0300

    issue #3: Adding unit tests to locality model

--- spec/models/locality_spec.rb
+++ spec/models/locality_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Locality, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Should create Locality with' do
+    before(:each) do
+        @local = Locality.new(local_one: 'A', local_two: 'B', distance: 5)
+    end
+
+    it 'valid data' do
+      @local.save()
+      expect(@local).to be_valid
+    end
+
+    it 'null local one' do
+      @local.local_one = nil
+      @local.save()
+      expect(@local).to_not be_valid
+    end
+
+    it 'null local two' do
+      @local.local_two = nil
+      @local.save()
+      expect(@local).to_not be_valid
+    end
+
+    it 'null distance' do
+      @local.distance = nil
+      @local.save()
+      expect(@local).to_not be_valid
+    end
+
+    it 'no numeric distance' do
+      @local.distance = 'text'
+      @local.save()
+      expect(@local).to_not be_valid
+    end
+
+    it 'no integer distance' do
+      @local.distance = 2.55
+      @local.save()
+      expect(@local).to_not be_valid
+    end
+  end
 end
