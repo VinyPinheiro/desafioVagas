5a88eac1c183cebec45b934f836baa294115cdf3
VinyPinheiro <viny-pinheiro@hotmail.com>
2019-07-15 22:16:19 -0300

    issue #3: Create route to register people

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    post 'people/create', 'people#create'
+    post 'pessoas' => 'people#create'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
