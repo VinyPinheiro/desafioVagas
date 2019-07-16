f98fc83450c4681a0d37242cbe8ad95a8f8b0c76
VinyPinheiro <viny-pinheiro@hotmail.com>
2019-07-16 20:37:23 -0300

    issue #5: adding validation to same application for job

--- app/models/application_for_job.rb
+++ app/models/application_for_job.rb
@@ -4,7 +4,9 @@ class ApplicationForJob < ApplicationRecord
 
   PERSON_CANT_BLANK = 'Preencha o código da pessoa'.freeze
   JOB_CANT_BLANK = 'Preencha o código da vaga'.freeze
+  EXISTENT = 'Candidatura já efetuada'
 
   validates :job, presence: { message: PERSON_CANT_BLANK }
   validates :person, presence: { message: JOB_CANT_BLANK }
+  validates :person, uniqueness: { scope: :job, message: EXISTENT }
 end
--- spec/models/application_for_job_spec.rb
+++ spec/models/application_for_job_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe ApplicationForJob, type: :model do
     describe 'Should create Person with' do
       before(:each) do
         edgeA = Edge.create(name: 'A')
-        person = Person.new(name: 'teste', occupation: 'ocupacao teste',
-                            edge: edgeA, level: 1)
-        job = Job.new(company: 'teste', title: 'ocupacao teste',
-                      description: 'Descrição teste', edge: edgeA, level: 1)
-        @application = ApplicationForJob.new(person: person, job: job)
+        @person = Person.new(name: 'teste', occupation: 'ocupacao teste',
+                             edge: edgeA, level: 1)
+        @job = Job.new(company: 'teste', title: 'ocupacao teste',
+                       description: 'Descrição teste', edge: edgeA, level: 1)
+        @application = ApplicationForJob.new(person: @person, job: @job)
       end
 
       it 'valid data' do
@@ -20,6 +20,12 @@ RSpec.describe ApplicationForJob, type: :model do
         @application.person = nil
         @application.save()
         expect(@application).to_not be_valid
+      end
+
+      it 'existent application' do
+        @application.save()
+        application = ApplicationForJob.new(person: @person, job: @job)
+        expect(application).to_not be_valid
       end
 
       it 'null job' do
